4b72ed632f596a7c652028760f123370baf45e6c
KeMarcY <kele.marton.robert@gmail.com>
2025-10-08 13:42:00 +0200

    fix-apiDocs

--- Backend/cQuizy/cQuizy/urls.py
+++ Backend/cQuizy/cQuizy/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .api import api # Global API
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api.urls),
     path('', include('website.urls')),
-    path('users/', include('users.urls')),
 ]
--- Backend/cQuizy/users/api.py
+++ Backend/cQuizy/users/api.py
@@ -1,4 +1,4 @@
-from ninja import NinjaAPI, Schema
+from ninja import Router
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
@@ -7,9 +7,10 @@ from django.views.decorators.csrf import csrf_protect
 from django.db import IntegrityError
 from pydantic import EmailStr, field_validator
 
-api = NinjaAPI()
+#! Instead of NinjaAPI, we use Router
+router = Router(tags=['users'])  # The 'tags' are great for organizing docs
 
 #* Test Endpoint ##################################################
-@api.get("/hello")
-def hello(request, name: str):
-    return {"message": f"Hai {name}, welcome to DUwUD 😎"}
+@router.get("/hello")
+def hello(request):
+    return {"message": "Hello, your API works!"}
